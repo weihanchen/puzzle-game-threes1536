f990e4ed2735c5bf4f73fdd883d492f7cd793351
weihanchen <willhanchen@gmail.com>
2016-09-28 14:53:45 +0800

    add logic js file

--- run.js
+++ run.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-app.use(express.static('./docs'));
+app.use(express.static('.'));
 
 app.listen(8000, function () {
   console.log('Example app listening on port 8000!');
