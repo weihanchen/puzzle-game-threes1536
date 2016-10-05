47263110df8ecdab042d7f092ef78315bd6552fc
weihanchen <willhanchen@gmail.com>
2016-10-06 00:19:55 +0800

    fix .travis.yml

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,12 +3,19 @@
 # variables
 SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
+TARGET_FOLDER="public"
+GITHUB_REPO="@github.com/weihanchen/puzzle-game-threes1536.git"
+FULL_REPO="https://$GITHUB_TOKEN$GITHUB_REPO"
+
+# initial
+rm -rf .git/
+git init
 
 # config
 git config --global user.email "${GITHUB_MAIL}"
 git config --global user.name "${GITHUB_USER}"
 
-# deploy
+#deploy
 git add .
 MESSAGE=`date +\ %Y-%m-%d\ %H:%M:%S`
 git commit -m "Site updated:${MESSAGE}"
